43d352e315d525b62d0c0e3ad921af3047b7187c
wojtek534 <wojtek.rzepkaa@gmail.com>
2017-10-24 21:43:07 +0200

    0.11

--- sandbox.js
+++ sandbox.js
@@ -1,32 +1,4 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
+MyEvent = (event) => {
+    event.preventDefault();
 
-class blablbal extends React.Component {
-    constructor(props, context) {
-        super(props, context)
-    }
-    
-    render() {
-        return (
-            <div></div>
-        );
-    }
-}
-
-blablbal.propTypes = {
-    //myProp: PropTypes.string.isRequired;
 };
-
-function mapStateToProps(state, ownProps) {
-    return {state: state};
-}
-
-function mapDispatchToProps(dispatch) {
-    return {
-        actions: bindActionCreators(YourImportedActions, dispatch) 
-    };
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(blablbal);
